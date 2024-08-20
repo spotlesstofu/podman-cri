8b7e104461b630662d3de443b7d4732d6bde819e
Camilla Conte <cconte@redhat.com>
2024-08-20 13:48:11 +0100

    complete proxy handler

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,39 +1,39 @@
 use axum::{
+    body::{to_bytes, Body, Bytes},
+    extract::Request,
     http::StatusCode,
     response::{IntoResponse, Response},
 };
-use hyper_util::client::legacy::Client;
 use http_body_util::Full;
-use hyper::body::Bytes;
-use hyper::Request;
+use hyper_util::client::legacy::Client;
 use hyperlocal::{UnixClientExt, UnixConnector, Uri};
 
-pub async fn reverse_proxy(mut req: axum::extract::Request<axum::body::Bytes>) -> Result<Response, StatusCode> {
+pub async fn reverse_proxy(req: Request<Body>) -> Result<Response, StatusCode> {
     let path = req.uri().path();
     let path_query = req
         .uri()
         .path_and_query()
         .map(|v| v.as_str())
         .unwrap_or(path);
-
     let uri = Uri::new("/run/user/1000/podman/podman.sock", path_query);
-    let client: Client<UnixConnector, Full<Bytes>> = Client::unix();
-
-    *req.uri_mut() = axum::http::uri::Uri::try_from(uri).unwrap();
 
     let (parts, body) = req.into_parts();
+    let bytes = to_bytes(body, usize::MAX).await.unwrap();
 
-    let req: Request<Full<Bytes>> = Request::builder()
-    .method(parts.method)
-    .uri(parts.uri)
-    .body(Full::from(body))
-    .expect("request builder");
+    let request: hyper::Request<Full<Bytes>> = hyper::Request::builder()
+        .method(parts.method)
+        .uri(uri)
+        .body(Full::from(bytes))
+        .expect("request builder");
 
     // todo: add parts.headers to req?
 
-    Ok(client
-        .request(req)
+    let client: Client<UnixConnector, Full<Bytes>> = Client::unix();
+    let response = client
+        .request(request)
         .await
         .map_err(|_| StatusCode::BAD_REQUEST)?
-        .into_response())
+        .into_response();
+
+    Ok(response)
 }
