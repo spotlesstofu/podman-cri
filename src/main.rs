a260a7329bb772c0ae3f12a32f8ceadff17a7737
Camilla Conte <cconte@redhat.com>
2024-09-12 13:10:46 +0100

    add route /cri/_ping

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,14 @@ async fn main() {
         .route("/containers/:name/json", get(handlers::container_inspect))
         .route("/containers/:name/stop", post(handlers::container_stop))
         // libpod containers routes
-        .route("/libpod/containers/json", get(handlers::container_list_libpod))
-        .route("/libpod/containers/create", post(handlers::container_create_libpod))
+        .route(
+            "/libpod/containers/json",
+            get(handlers::container_list_libpod),
+        )
+        .route(
+            "/libpod/containers/create",
+            post(handlers::container_create_libpod),
+        )
         // libpod pods routes
         .route("/libpod/pods/json", get(handlers::pod_list_libpod))
         .route("/libpod/pods/create", post(handlers::pod_create_libpod))
@@ -29,10 +35,12 @@ async fn main() {
         .route("/libpod/pods/:name", delete(handlers::pod_delete_libpod))
         // reply to ping
         .route("/_ping", get(handlers::ping))
+        .route("/cri/_ping", get(handlers::ping))
         // forward to podman all the non-matching paths
         .fallback(reverse_proxy);
 
-    let path = std::env::var("PODMAN_CRI_ENDPOINT").unwrap_or("/run/user/1000/podman/podman-cri.sock".into());
+    let path = std::env::var("PODMAN_CRI_ENDPOINT")
+        .unwrap_or("/run/user/1000/podman/podman-cri.sock".into());
 
     serve(app, path).await;
 }
