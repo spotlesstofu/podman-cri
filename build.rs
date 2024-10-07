c243b3cb1911eb6a7d7c79d351d3299f475d5879
Camilla Conte <cconte@redhat.com>
2024-10-07 15:11:26 +0100

    tonic_build: add derive default

--- build.rs
+++ build.rs
@@ -3,8 +3,8 @@ use std::io::Result;
 fn main() -> Result<()> {
     tonic_build::configure()
         .build_server(false)
-        // derive serialize to support json
-        .type_attribute(".", "#[derive(serde::Serialize)]")
+        // derive serialize to support json, derive default for easier coding
+        .type_attribute(".", "#[derive(serde::Serialize, Default)]")
         .type_attribute(".", "#[serde(rename_all = \"PascalCase\")]")
         .compile(&["proto/runtime.proto"], &["proto"])?;
     Ok(())
