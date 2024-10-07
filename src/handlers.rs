0ba218cf917ee3e8ac025e2fd2a7ab41b7ede641
Camilla Conte <cconte@redhat.com>
2024-10-07 12:35:32 +0100

    add fn get_image_client

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,6 +1,7 @@
+use std::error::Error;
+
 use axum::{extract::Path, http::StatusCode, Json};
 use tokio::net::UnixStream;
-
 use tonic::{
     transport::{Channel, Endpoint, Uri},
     Request,
@@ -10,24 +11,35 @@ mod cri {
     tonic::include_proto!("runtime.v1");
 }
 
-use cri::runtime_service_client::RuntimeServiceClient;
+use cri::{image_service_client::ImageServiceClient, runtime_service_client::RuntimeServiceClient};
 
 use podman_api::models::{
     Container, ContainerCreateResponse, ContainerJson, CreateContainerConfig, IdResponse,
     ListContainer, ListPodsReport, PodRmReport, PodSpecGenerator, PodStartReport, PodStopReport,
 };
 
-/// Get a client to connect to a CRI server (for example, CRI-O).
-async fn get_client() -> Result<RuntimeServiceClient<Channel>, Box<dyn std::error::Error>> {
+async fn get_channel() -> Result<Channel, Box<dyn Error>> {
     // We will ignore the http uri and connect to the Unix socket.
     let channel = Endpoint::try_from("http://[::]:50051")?
         .connect_with_connector(tower::service_fn(|_: Uri| {
-            let path = std::env::var("CONTAINER_RUNTIME_ENDPOINT").unwrap_or("/run/crio/crio.sock".into());
+            let path =
+                std::env::var("CONTAINER_RUNTIME_ENDPOINT").unwrap_or("/run/crio/crio.sock".into());
             UnixStream::connect(path)
         }))
         .await?;
+    Ok(channel)
+}
 
+/// Get a client to connect to a CRI server (for example, CRI-O).
+async fn get_client() -> Result<RuntimeServiceClient<Channel>, Box<dyn Error>> {
+    let channel = get_channel().await?;
     let client = RuntimeServiceClient::new(channel);
+    Ok(client)
+}
+
+async fn get_image_client() -> Result<ImageServiceClient<Channel>, Box<dyn Error>> {
+    let channel = get_channel().await?;
+    let client = ImageServiceClient::new(channel);
     Ok(client)
 }
 
