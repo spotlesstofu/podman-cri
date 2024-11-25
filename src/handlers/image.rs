0404e47cea4e5a4dc698c6acef4021d7f3f9df46
Camilla Conte <cconte@redhat.com>
2024-11-25 10:51:49 +0100

    image: don't pull when not needed

--- src/handlers/image.rs
+++ src/handlers/image.rs
@@ -4,15 +4,17 @@ use tonic::Request;
 
 use podman_api::models::{ImageCreateQueryParams, ImageListLibpodQueryParams, LibpodImageSummary};
 
-use crate::cri;
+use crate::cri::{self, ImageFilter};
 use crate::cri_clients::get_image_client;
 
 // POST /images/create
 pub async fn image_create(Json(params): Json<ImageCreateQueryParams>) -> String {
     if params.from_image.is_some() {
         let image = params.from_image.unwrap();
-        let tag = params.tag.expect("image tag or digest to pull");
-        image_pull(image, tag).await
+        let user_specified_image = params.tag.expect("image tag or digest to pull");
+        pull_image_or_local(image, user_specified_image)
+            .await
+            .unwrap()
     } else if params.from_src.is_some() {
         // TODO proxy to podman to build the image, then copy the image to CRI-O.
         unimplemented!("build image");
@@ -21,29 +23,63 @@ pub async fn image_create(Json(params): Json<ImageCreateQueryParams>) -> String 
     }
 }
 
-async fn image_pull(image: String, tag: String) -> String {
-    let client = get_image_client();
+async fn pull_image(image_spec: cri::ImageSpec) -> Result<String, tonic::Status> {
+    let client = get_image_client().await;
 
     let message = cri::PullImageRequest {
-        image: Some(cri::ImageSpec {
-            image,
-            user_specified_image: tag,
-            ..Default::default()
-        }),
+        image: Some(image_spec),
         auth: None,
         sandbox_config: None,
     };
-
     let request = Request::new(message);
-    let response = client
-        .await
-        .unwrap()
-        .pull_image(request)
-        .await
-        .unwrap()
-        .into_inner();
 
-    response.image_ref
+    let response = client.unwrap().pull_image(request).await?;
+    let image_ref = response.into_inner().image_ref;
+
+    Ok(image_ref)
+}
+
+async fn get_local_image(image_spec: cri::ImageSpec) -> Result<String, tonic::Status> {
+    let filter = cri::ImageFilter {
+        image: Some(image_spec),
+    };
+
+    let images = list_images(Some(filter)).await;
+
+    match images.first() {
+        Some(image) => Ok(image.id.clone()),
+        None => Err(tonic::Status::not_found("no such image")),
+    }
+}
+
+async fn pull_image_or_local(
+    image: String,
+    user_specified_image: String,
+) -> Result<String, tonic::Status> {
+    let image_spec = cri::ImageSpec {
+        image,
+        user_specified_image,
+        ..Default::default()
+    };
+
+    let local_result = get_local_image(image_spec.clone()).await;
+
+    if image_spec.user_specified_image.starts_with("sha") && local_result.is_ok() {
+        return local_result;
+    }
+
+    let pull_result = pull_image(image_spec).await;
+
+    if pull_result.is_ok() {
+        return pull_result;
+    }
+
+    if local_result.is_ok() {
+        tracing::debug!("couldn't pull image, using local");
+        local_result
+    } else {
+        pull_result
+    }
 }
 
 impl From<cri::Image> for LibpodImageSummary {
