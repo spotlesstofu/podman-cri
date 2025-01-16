a13be1bcd5b270986ee65921566bb7e24d27972e
Camilla Conte <cconte@redhat.com>
2025-01-16 16:26:25 +0100

    extension

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,25 +1,41 @@
 import type { ExtensionContext } from '@podman-desktop/api';
 import * as extensionApi from '@podman-desktop/api';
 
-/**
- * Below is the "typical" extension.ts file that is used to activate and deactivate the extension.
- * this file as well as package.json are the two main files that are required to develop a Podman Desktop extension.
- */
+const machineImage = "quay.io/spotlesstofu/podman-cri:5.1"
+
+async function execPodman(args) {
+  var command = "podman";
+  return await extensionApi.process.exec(command, args)
+}
 
 // Initialize the activation of the extension.
 export async function activate(extensionContext: ExtensionContext): Promise<void> {
-  var tag = extensionApi.version;
-  var image = `quay.io/spotlesstofu/podman-cri:${tag}`;
-  var command = "podman"; 
-  var args = ["machine", "os", "apply", "--restart", image];
+  // machine init
   try {
-    await extensionApi.process.exec(command, args);
+    await execPodman(["machine", "init", "--now"]);
   } catch (e) {
-    console.error(e)
+    if (e.stderr.includes("already exists")) {
+        // pass, no need to create the machine
+    } else {
+      console.error(e.stderr);
+      extensionApi.window.showWarningMessage(`Failed machine init: ${e.stderr}`);
+      throw e;
+    }
+  }
+
+  // TODO await machine is up
+
+  // machine os apply
+  try {
+    await execPodman(["machine", "os", "apply", "--restart", machineImage]);
+  } catch (e) {
+    console.error(e.stderr);
+    extensionApi.window.showWarningMessage(`Failed machine os apply: ${e.stderr}`);
+    throw e;
   }
 }
 
 // Deactivate the extension
 export async function deactivate(): Promise<void> {
-  console.log('stopping hello world extension');
+  extensionApi.window.showWarningMessage("To fully deactivate the extension, reset the Podman Machine by running the following command:\n\n    podman machine reset");
 }
